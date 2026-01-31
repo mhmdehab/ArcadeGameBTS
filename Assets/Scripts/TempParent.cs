63c1fb4491983f7a4df00ca98ea00d7ab287f823
ramezyoussef <ramez_youssef123@outlook.com>
2026-01-31 21:56:31 +0200

    can throw can pick drop

---
+++ Assets/Scripts/TempParent.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TempParent : MonoBehaviour
+{
+ public static TempParent Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance==null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
