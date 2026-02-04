ac313b2cd2d563eb5bba6b0d5c162f1a9fec38b1
ramezyoussef <ramez_youssef123@outlook.com>
2026-02-04 15:53:58 +0200

    tree is drawn

--- Assets/Scripts/TreeSpawner.cs
+++ Assets/Scripts/TreeSpawner.cs
@@ -1,12 +1,20 @@
+using UnityEditor;
 using UnityEngine;
 
 public class TreeSpawner : MonoBehaviour 
 {
-    
+    private GameObject currentTree=null;
+    public GameObject Tree1;
+    public GameObject Tree2;
+    public GameObject Tree3;
+    public Transform treeSpawnPoint;
+
+    private bool playerInZone = false;
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            playerInZone = true;
             Debug.Log("player entered");
         }
         
@@ -15,8 +23,33 @@ public class TreeSpawner : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            playerInZone = false;
             Debug.Log("player left");
         }
         
+    }
+
+    private void Update()
+    {
+        if (playerInZone && currentTree==null)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                currentTree=Instantiate(Tree1 , treeSpawnPoint.position,treeSpawnPoint.rotation);
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                currentTree=Instantiate(Tree2,treeSpawnPoint.position,treeSpawnPoint.rotation);
+            }
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                currentTree=Instantiate(Tree3,treeSpawnPoint.position,treeSpawnPoint.rotation);
+            }
+        }
+        if (currentTree!=null &&Input.GetKeyDown(KeyCode.X))
+        {
+            Destroy(currentTree);
+            currentTree=null;
+        }
     }
 }
